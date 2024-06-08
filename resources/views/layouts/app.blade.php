a03c36da6b6001894105ed745f437b4be7c0b720
Fikri Mubarok <fikrimubarok111@gmail.com>
2024-06-08 16:35:27 +0700

    app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -136,15 +136,24 @@
       @yield('content')
     </div>
   </div>
-  <script src="/assets/libs/jquery/dist/jquery.min.js"></script>
-  <script src="/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="/assets/js/sidebarmenu.js"></script>
-  <script src="/assets/js/app.min.js"></script>
-  <script src="/assets/libs/apexcharts/dist/apexcharts.min.js"></script>
-  <script src="/assets/libs/simplebar/dist/simplebar.js"></script>
-  <script src="/assets/js/dashboard.js"></script>
+  {{-- <link rel="stylesheet" href={{URL::asset("/css/sensorPage.css")}}> --}}
+  {{-- <script src="/assets/libs/jquery/dist/jquery.min.js"></script> --}}
+  <script src={{URL::asset("/assets/libs/jquery/dist/jquery.min.js")}}></script>
+  {{-- <script src="/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script> --}}
+  <script src={{URL::asset("/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js")}}></script>
+  {{-- <script src="/assets/js/sidebarmenu.js"></script> --}}
+  <script src={{URL::asset("/assets/js/sidebarmenu.js")}}></script>
+  {{-- <script src="/assets/js/app.min.js"></script> --}}
+  <script src={{URL::asset("/assets/js/app.min.js")}}></script>
+  {{-- <script src="/assets/libs/apexcharts/dist/apexcharts.min.js"></script> --}}
+  <script src={{URL::asset("/assets/libs/apexcharts/dist/apexcharts.min.js")}}></script>
+  {{-- <script src="/assets/libs/simplebar/dist/simplebar.js"></script> --}}
+  <script src={{URL::asset("/assets/libs/simplebar/dist/simplebar.js")}}></script>
+  {{-- <script src="/assets/js/dashboard.js"></script> --}}
+  <script src={{URL::asset("/assets/js/dashboard.js")}}></script>
   <!-- solar icons -->
-  <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+  {{-- <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script> --}}
+  <script src={{URL::asset("https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js")}}></script>
 </body>
 
 </html>
