324ada6634f6ad526bc91f1bf3a4899d90241054
Fikri Mubarok <fikrimubarok111@gmail.com>
2024-06-08 17:09:21 +0700

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>GreenHouse-IoT</title>
-  <link rel="shortcut icon" type="image/png" href="/assets/images/logos/favicon.png" />
+  {{-- <link rel="shortcut icon" type="image/png" href="/assets/images/logos/favicon.png" /> --}}
   {{-- <link rel="stylesheet" href="/assets/css/styles.min.css" /> --}}
+  <link rel="shortcut icon" type="image/png" href="{{ URL::asset('assets/images/logos/favicon.png') }}" />
   <link rel="stylesheet" href={{URL::asset("assets/css/styles.min.css")}}>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" />
 </head>
