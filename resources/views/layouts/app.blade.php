55d5115ee4120eb4dae46d8aa4bc8ef8a5afe220
Fikri Mubarok <fikrimubarok111@gmail.com>
2024-06-09 12:01:47 +0700

    app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" href="{{ asset("assets/css/styles.min.css") }}" />
   <link rel="stylesheet" href="http://datapanel.fikrimubarok5.web.id/GreenHouse-IoT/public/assets/css/styles.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
