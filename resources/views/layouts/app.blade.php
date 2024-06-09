d9d1309e33255e0794c21e59357d6767f3457e38
Fikri Mubarok <fikrimubarok111@gmail.com>
2024-06-09 12:49:28 +0700

    bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
   <link rel="shortcut icon" type="image/png" href="{{ asset("assets/images/logos/favicon.png") }}" />
   <link rel="stylesheet" href="{{ asset("assets/css/styles.min.css") }}" />
   <link rel="stylesheet" href="http://datapanel.fikrimubarok5.web.id/GreenHouse-IoT/public/assets/css/styles.min.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset("assets/dist/css/bootstrap.min.css") }}" />
+  <link href="{{ asset("assets/dist/css/bootstrap.min.css") }}" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
