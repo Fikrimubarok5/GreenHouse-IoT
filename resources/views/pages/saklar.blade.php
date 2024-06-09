6f7bddb5a44a564dea74d1c2b0a8c4e970059ce3
Fikri Mubarok <fikrimubarok111@gmail.com>
2024-06-09 23:09:43 +0700

    saklar

--- resources/views/pages/saklar.blade.php
+++ resources/views/pages/saklar.blade.php
@@ -55,22 +55,14 @@
     </script>
 <div class="body-wrapper-inner">
     <div class="container-fluid">
-        {{-- <!-- <div class="mb-3 row">
-            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Tambah Data</button>
-        </div> --> --}}
         <div class="row">
             <div class="d-flex justify-content-between">
                 <div class="card w-50 me-2">
                     <div class="card-body">
-                        <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
-                            <div class="mb-3 mb-sm-0">
-                                {{-- <h1 class="card-title fw-semibold">Button</h1> --}}
-                            </div>
-                        </div>
                         <div class="switch-container d-flex">
                             <div class="form-check form-switch" style="margin-left: 20px; flex: 1;">
                                 <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault1">
-                                <label class="form-check-label" for="flexSwitchCheckDefault1" id="switchLabel1">Water Pump</label>
+                                <label class="form-check-label" for="flexSwitchCheckDefault1" id="switchLabel1">Lamp Indicator</label>
                             </div>
                         </div>
                     </div>
@@ -78,14 +70,11 @@
                 <div class="card w-50 ms-2">
                     <div class="card-body">
                         <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
-                            <div class="mb-3 mb-sm-0">
-                                {{-- <h1 class="card-title fw-semibold">Saklar</h1> --}}
-                            </div>
                         </div>
                         <div class="switch-container d-flex">
                             <div class="form-check form-switch" style="margin-left: 20px; flex: 1;">
                                 <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault2">
-                                <label class="form-check-label" for="flexSwitchCheckDefault2" id="switchLabel2">Lamp Indicator</label>
+                                <label class="form-check-label" for="flexSwitchCheckDefault2" id="switchLabel2">Water Pump</label>
                             </div>
                         </div>
                     </div>
