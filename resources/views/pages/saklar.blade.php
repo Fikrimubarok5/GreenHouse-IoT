fa49ce89c78ea9aaa9f5047dba6642aa0dff91de
Fikri Mubarok <fikrimubarok111@gmail.com>
2024-06-10 22:06:34 +0700

    saklar

--- resources/views/pages/saklar.blade.php
+++ resources/views/pages/saklar.blade.php
@@ -7,9 +7,9 @@
             <div class="d-flex justify-content-between">
                 <div class="card w-50 me-2">
                     <div class="card-body">
-                        <div class="switch-container d-flex justify-content-between mb-9">
+                        <div class="switch-container d-flex ">
                             <div class="form-check form-switch" style="margin-left: 20px; flex: 1;">
-                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault1">
+                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault1" onchange="controlSaklar('led', this.checked ? 'on' : 'off')">
                                 <label class="form-check-label" for="flexSwitchCheckDefault1" id="switchLabel1">Lamp Indicator</label>
                             </div>
                         </div>
@@ -21,7 +21,7 @@
                         </div>
                         <div class="switch-container d-flex">
                             <div class="form-check form-switch" style="margin-left: 20px; flex: 1;">
-                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault2">
+                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault2" onchange="controlDevice('relay', this.checked ? 'on' : 'off')">
                                 <label class="form-check-label" for="flexSwitchCheckDefault2" id="switchLabel2">Water Pump</label>
                             </div>
                         </div>
@@ -30,9 +30,17 @@
             </div>
         </div>
     </div>
-    </div>
-        <div class="px-6 py-6 text-center">
-            <p class="mb-0 fs-4">Developed by <a href="https://www.instagram.com/fikrimubarok05/" target="_blank"
-                class="pe-1 text-primary text-decoration-underline">@fikrimubarok05</a> 2024</p>
-        </div>
+</div>
+<div class="px-6 py-6 text-center">
+    <p class="mb-0 fs-4">Developed by <a href="https://www.instagram.com/fikrimubarok05/" target="_blank"
+        class="pe-1 text-primary text-decoration-underline">@fikrimubarok05</a> 2024</p>
+</div>
+<script>
+    function controlDevice(device, status) {
+        fetch(`/device/${device}/${status}`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+    }
+</script>
 @endsection
